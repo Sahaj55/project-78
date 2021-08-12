50b474183c2f392b1e1abe3f6215b8bfba673ada
Sahaj55 <86662716+Sahaj55@users.noreply.github.com>
2021-08-12 16:41:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+var images = [
+    "https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"
+
+
+
+
+
+
+
+
+    ];
+    
+    
+    
+    
+    var i=0;
+    function changeslide()
+    {
+    
+      document.getElementById("monkey").src = images[i];
+      i++;
+    }
